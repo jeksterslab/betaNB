eb2f56ab27e8bd466cc27d071c8c7ae5c5cc9327
jeksterslab <jeksterslab@users.noreply.github.com>
2026-06-12 04:12:48 +0000

    Automated build [skip ci].
    Files changed:
    A	.Rbuildignore
    A	.setup/build/betaNB.pdf
    A	.setup/build/betaNB_1.0.7.9000.tar.gz
    A	.setup/latex/bib/quarto.bib
    A	CITATION.cff
    M	DESCRIPTION
    A	LICENSE
    A	LICENSE.md
    A	NAMESPACE
    A	R/dataSets-nas1982.R
    A	R/linearAlgebra-d-mat-dot.R
    A	R/linearAlgebra-pinv-of-d-mat-dot.R
    A	R/linearAlgebra-vec-dot.R
    A	R/linearAlgebra-vech-dot.R
    A	R/nBootstrap-acceleration-dot.R
    A	R/nBootstrap-bc-ci-dot.R
    A	R/nBootstrap-bc-probs-dot.R
    A	R/nBootstrap-bca-ci-dot.R
    A	R/nBootstrap-bca-probs-dot.R
    A	R/nBootstrap-ci-format-dot.R
    A	R/nBootstrap-pc-ci-dot.R
    A	R/nBootstrap-pc-probs-dot.R
    A	R/nBootstrap-z0-dot.R
    A	R/nBootstrap-z1-dot.R
    A	R/processLM-dif-dot.R
    A	R/processLM-process-lm-dot.R
    A	R/rhoMatrix-rho-of-sigma-dot.R
    A	R/strRegression-beta-star-of-rho-dot.R
    A	R/strRegression-beta-star-of-sigma-dot.R
    A	R/strRegression-p-cor-sq-dot.R
    A	R/strRegression-r-sq-bar-dot.R
    A	R/strRegression-r-sq-of-sigma-dot.R
    A	R/strRegression-s-p-cor-dot.R
    A	README.md
    M	data/nas1982.rda
    A	man/BetaNB.Rd
    A	man/DeltaRSqNB.Rd
    A	man/DiffBetaNB.Rd
    A	man/NB.Rd
    A	man/PCorNB.Rd
    A	man/RSqNB.Rd
    A	man/SCorNB.Rd
    A	man/betaNB-package.Rd
    A	man/coef.betanb.Rd
    A	man/confint.betanb.Rd
    A	man/nas1982.Rd
    A	man/print.betanb.Rd
    A	man/print.nb.Rd
    A	man/summary.betanb.Rd
    A	man/vcov.betanb.Rd
    M	project.Rproj
    A	vignettes/example-beta-nb.Rmd
    A	vignettes/example-delta-r-sq-nb.Rmd
    A	vignettes/example-diff-beta-nb.Rmd
    A	vignettes/example-p-cor-nb.Rmd
    A	vignettes/example-r-sq-nb.Rmd
    A	vignettes/example-s-cor-nb.Rmd
    A	vignettes/session.Rmd
    A	vignettes/vignettes.bib

---
+++ R/nBootstrap-z1-dot.R
@@ -0,0 +1,15 @@
+#' z1
+#'
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @param probs Numeric vector.
+#'   Vector of probabilities corresponding to alpha level.
+#'
+#' @return Returns a numeric vector.
+#'
+#' @family Confidence Intervals Functions
+#' @keywords nBootstrap ci internal
+#' @noRd
+.Z1 <- function(probs) {
+  stats::qnorm(p = probs)
+}
